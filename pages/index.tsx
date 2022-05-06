79085cde6cac4d82245994d2826469308214456b
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-06 12:55:06 -0400

    implemented ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+
+  console.log(tweets)
   return (
     <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
       <Head>
@@ -24,3 +32,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const tweets = await fetchTweets();
+
+  return {
+    props: {
+      tweets,
+    },
+  };
+}
